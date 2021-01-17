36eb626a14c65e2178c4a01b83a5e0e0fea13ff4
Sujay Samanta <sujayme@yahoo.co.in>
2021-01-18 01:48:53 +0530

    Added formatted code

--- src/main/java/com/img/vs/code/start/issiteup/IsSiteUpApplication.java
+++ src/main/java/com/img/vs/code/start/issiteup/IsSiteUpApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class IsSiteUpApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(IsSiteUpApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(IsSiteUpApplication.class, args);
+  }
 }
--- src/main/java/com/img/vs/code/start/issiteup/controller/UrlCheckController.java
+++ src/main/java/com/img/vs/code/start/issiteup/controller/UrlCheckController.java
@@ -4,9 +4,6 @@ import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-
-import javax.print.DocFlavor.STRING;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,38 +13,37 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 class UrlCheckController {
 
-    private static final String IS_SITE_UP = "Yeh! site is up!";
-    private static final String SITE_IS_DOWN = "Sorry! Site is down!";
-    private static final String INCORRECT_URL = "Url is incorrect!";
+  private static final String IS_SITE_UP = "Yeh! site is up!";
+  private static final String SITE_IS_DOWN = "Sorry! Site is down!";
+  private static final String INCORRECT_URL = "Url is incorrect!";
 
-    @GetMapping("/statusCheck")
-    public ResponseEntity<String> getStatus() {
-        return ResponseEntity.ok().body("Hey welcome to our new project developed using vs code");
+  @GetMapping("/statusCheck")
+  public ResponseEntity<String> getStatus() {
+    return ResponseEntity.ok().body("Hey welcome to our new project developed using vs code");
+  }
+
+  @GetMapping("/urlStatusCheck")
+  public String getUrlStatusCheck(@RequestParam String url) {
+
+    String returnMessage;
+
+    try {
+      URL urlObject = new URL(url);
+      HttpURLConnection conn = (HttpURLConnection) urlObject.openConnection();
+      conn.setRequestMethod("GET");
+      conn.connect();
+      if (HttpStatus.valueOf(conn.getResponseCode()).is4xxClientError()
+          || HttpStatus.valueOf(conn.getResponseCode()).is5xxServerError()) {
+        returnMessage = SITE_IS_DOWN;
+      } else {
+        returnMessage = IS_SITE_UP;
+      }
+    } catch (MalformedURLException e) {
+      returnMessage = INCORRECT_URL;
+    } catch (IOException e) {
+      returnMessage = SITE_IS_DOWN;
     }
 
-    @GetMapping("/urlStatusCheck")
-    public String getUrlStatusCheck(@RequestParam String url) {
-
-        String returnMessage = "";
-
-        try {
-            URL urlObject = new URL(url);
-            HttpURLConnection conn = (HttpURLConnection) urlObject.openConnection();
-            conn.setRequestMethod("GET");
-            conn.connect();
-            if(HttpStatus.valueOf(conn.getResponseCode()).is4xxClientError()
-            || HttpStatus.valueOf(conn.getResponseCode()).is5xxServerError()){
-                returnMessage = SITE_IS_DOWN;
-            }else{
-                returnMessage = IS_SITE_UP;
-            }
-        } catch (MalformedURLException e) {
-            returnMessage = INCORRECT_URL;
-        } catch (IOException e) {
-            returnMessage = SITE_IS_DOWN;
-        }
-
-        return returnMessage;
-    }
-
+    return returnMessage;
+  }
 }
