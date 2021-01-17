ec9cd441c9612de01185bebbfb97f57159b58df1
Sujay Samanta <sujaysamanta@Sujays-MacBook-Pro.local>
2021-01-18 01:41:13 +0530

    first commit

---
+++ src/test/java/com/img/vs/code/start/issiteup/IsSiteUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.img.vs.code.start.issiteup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsSiteUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
